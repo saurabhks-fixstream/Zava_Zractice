1e907e97c7cc3b1fb2353c77f3a15f9e2b8509f8
Saurabh <saurabh.singh@fixstream.com>
2019-02-17 20:35:42 +0530

    Solved CousinsinBinaryTree and RotatedDigits

---
+++ src/com/leetcode/javapractice/CousinsinBinaryTree.java
@@ -0,0 +1,40 @@
+package com.leetcode.javapractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CousinsinBinaryTree {
+
+    static Map<Integer, Integer> depth;
+    static Map<Integer, TreeNode> parent;
+
+    public static boolean isCousins(TreeNode root, int x, int y) {
+        depth = new HashMap<Integer, Integer>();
+        parent = new HashMap<Integer, TreeNode>();
+        dfs(root, null);
+        return depth.get(x) == depth.get(y) && parent.get(x) != parent.get(y);
+
+    }
+
+    private static void dfs(TreeNode root, TreeNode par) {
+        if (root != null) {
+            depth.put(root.val, par != null ? 1 + depth.get(par.val)
+                                            : 0);
+            parent.put(root.val, par);
+            dfs(root.left, root);
+            dfs(root.right, root);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        System.out.println(isCousins(root, 5, 4));
+
+    }
+
+}
