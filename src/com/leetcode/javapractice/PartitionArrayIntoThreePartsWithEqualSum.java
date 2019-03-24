542cf265b411154debb72b90ee605fa9612b5844
Saurabh <saurabh.singh@fixstream.com>
2019-03-24 13:47:46 +0530

    Solved PairsofSongsWithTotalDurationsDivisibleby60 and PartitionArrayIntoThreePartsWithEqualSum

---
+++ src/com/leetcode/javapractice/PartitionArrayIntoThreePartsWithEqualSum.java
@@ -0,0 +1,35 @@
+package com.leetcode.javapractice;
+
+public class PartitionArrayIntoThreePartsWithEqualSum {
+
+    public static boolean canThreePartsEqualSum(int[] A) {
+        int sum = 0;
+        for (int a : A) {
+            sum += a;
+        }
+        if (sum % 3 != 0) {
+            return false;
+        }
+        int part = 0;
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            part += A[i];
+            if (part != sum / 3) {
+                continue;
+            }
+            if (++count == 3) {
+                return true;
+            }
+            part = 0;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] A = { 0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1 };
+        System.out.println(canThreePartsEqualSum(A));
+
+    }
+
+}
