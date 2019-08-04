92c4b5cfa5b9e2ecc0549ec77630ac40b090e411
Saurabh <saurabh.singh@fixstream.com>
2019-08-04 19:42:40 +0530

    Solved DistributeCandiestoPeople and DuplicateZeros

---
+++ src/com/leetcode/javapractice/DuplicateZeros.java
@@ -0,0 +1,40 @@
+package com.leetcode.javapractice;
+
+public class DuplicateZeros {
+
+    public static void duplicateZeros(int[] arr) {
+        int zeros = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                zeros++;
+            }
+        }
+        int length = arr.length + zeros;
+        for (int i = arr.length - 1, j = length - 1; i < j; i--, j--) {
+            if (arr[i] != 0) {
+                if (j < arr.length) {
+                    arr[j] = arr[i];
+                }
+            } else {
+                if (j < arr.length) {
+                    arr[j] = arr[i];
+                }
+                j--;
+                if (j < arr.length) {
+                    arr[j] = arr[i];
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 0, 2, 3, 0, 4, 5, 0 };
+        duplicateZeros(arr);
+        for (int i : arr) {
+            System.out.print(i + ",");
+        }
+
+    }
+
+}
