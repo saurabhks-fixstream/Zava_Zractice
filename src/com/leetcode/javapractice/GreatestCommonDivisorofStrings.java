eb9684257264033cc4286f552f9bc399c13c2a7f
Saurabh <saurabh.singh@fixstream.com>
2019-08-05 21:17:51 +0530

    Solved RemoveAllAdjacentDuplicatesInString and GreatestCommonDivisorofStrings

---
+++ src/com/leetcode/javapractice/GreatestCommonDivisorofStrings.java
@@ -0,0 +1,23 @@
+package com.leetcode.javapractice;
+
+public class GreatestCommonDivisorofStrings {
+
+    public static String gcdOfStrings(String str1, String str2) {
+        if (str1.length() < str2.length()) {
+            return gcdOfStrings(str2, str1);
+        } else if (!str1.startsWith(str2)) {
+            return "";
+        } else if (str2.isEmpty()) {
+            return str1;
+        } else {
+            return gcdOfStrings(str1.substring(str2.length()), str2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(gcdOfStrings("ABCABC", "ABC"));
+
+    }
+
+}
