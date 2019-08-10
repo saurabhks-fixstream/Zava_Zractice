6ea694d65cfb93f16a0a60a7f99693d6cc1f6cb3
Saurabh <saurabh.singh@fixstream.com>
2019-08-10 18:26:59 +0530

    Solved SortArrayByParity and SortArrayByParityII

---
+++ src/com/leetcode/javapractice/SortArrayByParity.java
@@ -0,0 +1,38 @@
+package com.leetcode.javapractice;
+
+public class SortArrayByParity {
+
+    public static int[] sortArrayByParity(int[] A) {
+        int start = 0;
+        int end = A.length - 1;
+        while (start < end) {
+            if (A[start] % 2 != 0 && A[end] % 2 == 0) {
+                swap(A, start, end);
+                start++;
+                end--;
+            } else if (A[start] % 2 == 0) {
+                start++;
+            } else if (A[end] % 2 != 0) {
+                end--;
+            }
+        }
+        return A;
+    }
+
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] A = { 3, 1, 2, 4 };
+        sortArrayByParity(A);
+        for (int i : A) {
+            System.out.print(i + ",");
+        }
+
+    }
+
+}
