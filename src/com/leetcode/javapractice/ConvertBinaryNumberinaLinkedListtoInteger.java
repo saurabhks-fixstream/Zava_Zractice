d15190ccc4ce3dd595338117ff60abc293214377
Saurabh <saurabh.singh@fixstream.com>
2019-12-15 20:36:29 +0530

    Solved ConvertBinaryNumberinaLinkedListtoInteger and ElementAppearingMoreThan25InSortedArray

---
+++ src/com/leetcode/javapractice/ConvertBinaryNumberinaLinkedListtoInteger.java
@@ -0,0 +1,26 @@
+package com.leetcode.javapractice;
+
+public class ConvertBinaryNumberinaLinkedListtoInteger {
+
+    public static int getDecimalValue(ListNode head) {
+        if (head == null) {
+            return 0;
+        }
+        int result = 0;
+        while (head != null) {
+            result = result * 2 + head.val;
+            head = head.next;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+        System.out.println(getDecimalValue(head));
+
+    }
+
+}
