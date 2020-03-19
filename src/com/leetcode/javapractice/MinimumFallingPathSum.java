a3b8a8e15ed2ebaa77fa631c9fc3b05b4f7b04f5
Saurabh <saurabh.singh@fixstream.com>
2020-03-19 16:53:12 +0530

    Solved MinimumFallingPathSum and MinimumCostForTickets

---
+++ src/com/leetcode/javapractice/MinimumFallingPathSum.java
@@ -0,0 +1,33 @@
+package com.leetcode.javapractice;
+
+public class MinimumFallingPathSum {
+
+    public static int minFallingPathSum(int[][] A) {
+        int n = A.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int upLeft = A[i - 1][j == 0 ? 0
+                                             : j - 1]
+                             + A[i][j];
+                int up = A[i - 1][j] + A[i][j];
+                int upRight = A[i - 1][j == n - 1 ? j
+                                                  : j + 1]
+                              + A[i][j];
+                A[i][j] = Math.min(upLeft, Math.min(up, upRight));
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, A[n - 1][i]);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] A = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println(minFallingPathSum(A));
+
+    }
+
+}
