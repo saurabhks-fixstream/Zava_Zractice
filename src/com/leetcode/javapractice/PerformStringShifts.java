a4f0ce966490cee386d33e82e140b9f8c79c0906
Saurabh <saurabh.singh@fixstream.com>
2020-04-17 10:37:10 +0530

    Solved HTMLEntityParser and ValidParenthesisString

--- src/com/leetcode/javapractice/PerformStringShifts.java
+++ src/com/leetcode/javapractice/PerformStringShifts.java
@@ -20,7 +20,7 @@ public class PerformStringShifts {
         return s;
     }
 
-    static String leftrotate(String str, int d) {
+    private static String leftrotate(String str, int d) {
         String ans = str.substring(d) + str.substring(0, d);
         return ans;
     }
