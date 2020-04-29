d055cdd8961fe91f93970d18041dd40ad275fdce
Saurabh <saurabh.singh@fixstream.com>
2020-04-29 12:24:29 +0530

    Solved FirstUniqueNumber and MaximumScoreAfterSplittingaString

---
+++ src/com/leetcode/javapractice/MaximumScoreAfterSplittingaString.java
@@ -0,0 +1,28 @@
+package com.leetcode.javapractice;
+
+public class MaximumScoreAfterSplittingaString {
+
+    public static int maxScore(String s) {
+        int zero = 0;
+        int one = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                one++;
+            } else {
+                zero++;
+            }
+            if (i != s.length() - 1) {
+                max = Math.max(zero - one, max);
+            }
+        }
+        return max + one;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(maxScore("0011"));
+
+    }
+
+}
