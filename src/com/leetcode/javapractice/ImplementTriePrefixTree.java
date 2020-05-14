d39146a3d9ed4357eadfac823e9923c7f0eaa03d
Saurabh <saurabh.singh@fixstream.com>
2020-05-14 17:58:52 +0530

    Solved ImplementTriePrefixTree and FindMinimuminRotatedSortedArrayII

---
+++ src/com/leetcode/javapractice/ImplementTriePrefixTree.java
@@ -0,0 +1,72 @@
+package com.leetcode.javapractice;
+
+class TrieNode {
+
+    TrieNode[] children;
+    boolean isEnd;
+
+    public TrieNode() {
+        this.children = new TrieNode[26];
+        this.isEnd = false;
+    }
+
+}
+
+class Trie {
+
+    TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+
+}
+
+public class ImplementTriePrefixTree {
+
+    public static void main(String[] args) {
+
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+        trie.insert("app");
+        System.out.println(trie.search("app"));
+
+    }
+
+}
