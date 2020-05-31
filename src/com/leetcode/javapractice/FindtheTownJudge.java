162c5ba1892e44032c50a58469649a9c86fc55d4
Saurabh <saurabh.singh@fixstream.com>
2020-05-31 13:58:34 +0530

    Solved CourseSchedule and EditDistance

--- src/com/leetcode/javapractice/FindtheTownJudge.java
+++ src/com/leetcode/javapractice/FindtheTownJudge.java
@@ -3,28 +3,17 @@ package com.leetcode.javapractice;
 public class FindtheTownJudge {
 
     public static int findJudge(int N, int[][] trust) {
-        if (N == 1) {
-            return 1;
+        int[] count = new int[N + 1];
+        for (int[] t : trust) {
+            count[t[0]]--;
+            count[t[1]]++;
         }
-        int[] trustedBy = new int[N];
-        int[] trusts = new int[N];
-        int max = -1;
-        int index = -1;
-        for (int i = 0; i < trust.length; i++) {
-            int j = trust[i][0] - 1;
-            int k = trust[i][1] - 1;
-            trusts[j]++;
-            trustedBy[k]++;
-            if (trustedBy[k] >= max) {
-                max = trustedBy[k];
-                index = k;
+        for (int i = 1; i <= N; i++) {
+            if (count[i] == N - 1) {
+                return i;
             }
         }
-        if (max != N - 1) {
-            return -1;
-        }
-        return trusts[index] == 0 ? index + 1
-                                  : -1;
+        return -1;
     }
 
     public static void main(String[] args) {
