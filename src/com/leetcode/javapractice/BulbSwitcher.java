588d8623fd78eec0eeb14231143c95f675038b2c
Saurabh <saurabh.singh@fixstream.com>
2020-06-22 11:50:49 +0530

    Solved AvoidFloodinTheCity and BulbSwitcher

---
+++ src/com/leetcode/javapractice/BulbSwitcher.java
@@ -0,0 +1,15 @@
+package com.leetcode.javapractice;
+
+public class BulbSwitcher {
+
+    public static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(bulbSwitch(30));
+
+    }
+
+}
