46998a560ae510c00990a438452ab02fb4253f9d
Saurabh <saurabh.singh@fixstream.com>
2020-06-26 15:22:54 +0530

    Solved ThreeSumClosest and ThreeSumSmaller

---
+++ src/com/leetcode/javapractice/ThreeSumClosest.java
@@ -0,0 +1,35 @@
+package com.leetcode.javapractice;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int lo = i + 1;
+            int hi = nums.length - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (Math.abs(target - sum) < Math.abs(diff)) {
+                    diff = target - sum;
+                }
+                if (sum < target) {
+                    lo++;
+                } else {
+                    hi--;
+                }
+            }
+        }
+        return target - diff;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = { -1, 2, 1, -4 };
+        System.out.println(threeSumClosest(nums, 1));
+
+    }
+
+}
