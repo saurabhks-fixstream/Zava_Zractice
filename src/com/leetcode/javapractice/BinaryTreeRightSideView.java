eda7c514b7ab68272688e0f90326856c914fc290
Saurabh <saurabh.singh@fixstream.com>
2020-06-30 13:43:07 +0530

    Solved Searcha2DMatrix and BinaryTreeRightSideView

---
+++ src/com/leetcode/javapractice/BinaryTreeRightSideView.java
@@ -0,0 +1,46 @@
+package com.leetcode.javapractice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+
+    public static List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == 0) {
+                    result.add(node.val);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        System.out.println(rightSideView(root));
+
+    }
+
+}
