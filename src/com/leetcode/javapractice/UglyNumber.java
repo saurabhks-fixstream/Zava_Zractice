652fe6acbf87c0e393f3a76a0b09640280a5be75
Saurabh <saurabh.singh@fixstream.com>
2020-07-05 12:56:27 +0530

    Solved UglyNumberII and CanMakeArithmeticProgressionFromSequence

--- src/com/leetcode/javapractice/UglyNumber.java
+++ src/com/leetcode/javapractice/UglyNumber.java
@@ -3,38 +3,32 @@ package com.leetcode.javapractice;
 public class UglyNumber {
 
     public static boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
         if (num == 1) {
             return true;
         }
-        if (num == 0) {
-            return false;
-        }
-        boolean ugly = true;
-        while (num != 1) {
-            if (num % 5 != 0 && num % 3 != 0 && num % 2 != 0 && num != 1) {
-                ugly = false;
-                break;
+        while (num > 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 5 == 0) {
+                num /= 5;
             } else {
-                if (num % 5 == 0) {
-                    num /= 5;
-                }
-                if (num % 3 == 0) {
-                    num /= 3;
-                }
-                if (num % 2 == 0) {
-                    num /= 2;
-                }
+                break;
             }
         }
-        return ugly;
+        return num == 1;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(isUgly(8));
-        System.out.println(isUgly(14));
-        System.out.println(isUgly(0));
+        System.out.println(isUgly(6));
 
     }
+
+    
 
 }
