03f6e20c566ee4fe5e5ffd0d0bf56b52a2999ec1
Saurabh <saurabh.singh@fixstream.com>
2020-07-07 17:04:22 +0530

    Solved FindKPairswithSmallestSums and MeetingRooms

--- src/com/leetcode/javapractice/PlusOne.java
+++ src/com/leetcode/javapractice/PlusOne.java
@@ -4,37 +4,22 @@ public class PlusOne {
 
     public static int[] plusOne(int[] digits) {
         int carry = 0;
-        int length = digits.length - 1;
-        int i;
-        for (i = length; i >= 0; i--) {
+        digits[digits.length - 1]++;
+        for (int i = digits.length - 1; i >= 0; i--) {
             int sum = 0;
-            if (i == length) {
-                sum += digits[i] + 1;
-            } else {
-                sum += digits[i] + carry;
-            }
-            if (sum >= 10) {
-                carry = 1;
-                digits[i] = sum - 10;
-            } else {
-                carry = 0;
-                digits[i] = sum;
-            }
+            sum += digits[i] + carry;
+            carry = sum / 10;;
+            digits[i] = sum % 10;
         }
-        if (carry == 1) {
-            length += 2;
-        } else {
-            length += 1;
-        }
+        int[] number = new int[carry == 1 ? digits.length + 1
+                                          : digits.length];
         int j = 0;
-        i = 0;
-        int[] number = new int[length];
         if (carry == 1) {
             number[0] = 1;
             j++;
         }
-        for (; j < length; j++) {
-            number[j] = digits[i++];
+        for (int i = 0; i < digits.length & j < number.length; i++, j++) {
+            number[j] = digits[i];
         }
         return number;
     }
