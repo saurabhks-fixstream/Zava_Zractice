f9e11b65c86ea25d119520625b10c82a6ae982d4
Saurabh Kumar Singh <saurabh.singh@fixstream.com>
2020-07-22 13:59:06 +0530

    Solved AverageofLevelsinBinaryTree and ConstructStringfromBinaryTree

---
+++ src/com/leetcode/javapractice/AverageofLevelsinBinaryTree.java
@@ -0,0 +1,46 @@
+package com.leetcode.javapractice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageofLevelsinBinaryTree {
+
+    public static List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0.0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(sum / size);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(averageOfLevels(root));
+
+    }
+
+}
