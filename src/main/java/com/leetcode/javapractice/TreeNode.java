0eee5fe60a591a8b7235da2b53f9233e12305d9d
Saurabh Kumar Singh <saurabh.ksingh.eee10@itbhu.ac.in>
2020-09-16 22:10:51 +0530

    Solved MaximumXORofTwoNumbersinanArray and InorderSuccessorinBSTII

--- src/main/java/com/leetcode/javapractice/TreeNode.java
+++ src/main/java/com/leetcode/javapractice/TreeNode.java
@@ -5,10 +5,12 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+    TreeNode parent;
 
     TreeNode(int val) {
         this.val = val;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 }
