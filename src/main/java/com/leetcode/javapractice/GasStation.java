0afe5df509c7814d753d29316f2370e2f0949a04
Saurabh Kumar Singh <saurabh.ksingh.eee10@itbhu.ac.in>
2020-09-23 15:15:39 +0530

    Solved InsertintoaSortedCircularLinkedList and GasStation

---
+++ src/main/java/com/leetcode/javapractice/GasStation.java
@@ -0,0 +1,28 @@
+package com.leetcode.javapractice;
+
+public class GasStation {
+
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        int sumGas = 0;
+        int sumCost = 0;
+        int start = 0;
+        int tank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sumGas += gas[i];
+            sumCost += cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return sumGas < sumCost ? -1 : start;
+    }
+
+    public static void main(String[] args) {
+
+        int[] gas = {1, 2, 3, 4, 5};
+        int[] cost = {3, 4, 5, 1, 2};
+        System.out.println(canCompleteCircuit(gas, cost));
+    }
+}
