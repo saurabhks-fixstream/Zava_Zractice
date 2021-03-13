041c8db0aa67f16e7e4cd839a3ff6e8dd0045f32
Saurabh Kumar Singh <saurabh.ksingh.eee10@itbhu.ac.in>
2021-03-13 16:26:50 +0530

    Solved ShortEncodingofWords and DesignHashMap

---
+++ src/main/java/com/leetcode/javapractice/ShortEncodingofWords.java
@@ -0,0 +1,28 @@
+package com.leetcode.javapractice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShortEncodingofWords {
+
+    public static int minimumLengthEncoding(String[] words) {
+        Set<String> good = new HashSet(Arrays.asList(words));
+        for (String word : words) {
+            for (int k = 1; k < word.length(); k++) {
+                good.remove(word.substring(k));
+            }
+        }
+        int ans = 0;
+        for (String word : good) {
+            ans += word.length() + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = {"time", "me", "bell"};
+        System.out.println(minimumLengthEncoding(words));
+    }
+}
