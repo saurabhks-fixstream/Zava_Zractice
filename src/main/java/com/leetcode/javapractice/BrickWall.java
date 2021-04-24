83a70f757ca304367bb0a5a9723fb37d937a3ae8
Saurabh Kumar Singh <saurabh.ksingh.eee10@itbhu.ac.in>
2021-04-24 21:43:27 +0530

    Solved Triangle and BrickWall

---
+++ src/main/java/com/leetcode/javapractice/BrickWall.java
@@ -0,0 +1,36 @@
+package com.leetcode.javapractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BrickWall {
+
+    public static int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int count = 0;
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                count = Math.max(count, map.get(sum));
+            }
+        }
+        return wall.size() - count;
+    }
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> wall = new ArrayList<List<Integer>>();
+        wall.add(Arrays.asList(1, 2, 2, 1));
+        wall.add(Arrays.asList(3, 1, 2));
+        wall.add(Arrays.asList(1, 3, 2));
+        wall.add(Arrays.asList(2, 4));
+        wall.add(Arrays.asList(3, 1, 2));
+        wall.add(Arrays.asList(1, 3, 1, 1));
+        System.out.println(leastBricks(wall));
+    }
+}
