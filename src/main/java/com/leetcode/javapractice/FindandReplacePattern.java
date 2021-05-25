74f57c52959820bc2a4bb42b5ebc2e53305da33a
Saurabh Kumar Singh <saurabh.ksingh.eee10@itbhu.ac.in>
2021-05-25 17:44:47 +0530

    Solved MinimumKnightMoves and FindtheShortestSuperstring

--- src/main/java/com/leetcode/javapractice/FindandReplacePattern.java
+++ src/main/java/com/leetcode/javapractice/FindandReplacePattern.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FindandReplacePatternSolution {
+public class FindandReplacePattern {
 
     public static List<String> findAndReplacePattern(String[] words, String pattern) {
         int[] p = findAndReplacePatternHelper(pattern);
