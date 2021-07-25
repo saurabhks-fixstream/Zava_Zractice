1c56a632bc542fc6d826478e8f0207afd57455a5
Saurabh Kumar Singh <saurabh.ksingh.eee10@itbhu.ac.in>
2021-07-25 18:41:04 +0530

    Solved ThreeEqualParts and ReverseNodesinkGroup

---
+++ src/main/java/com/leetcode/javapractice/ReverseNodesinkGroup.java
@@ -0,0 +1,38 @@
+package com.leetcode.javapractice;
+
+public class ReverseNodesinkGroup {
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+        if (count == k) {
+            curr = reverseKGroup(curr, k);
+            while (count-- > 0) {
+                ListNode tmp = head.next;
+                head.next = curr;
+                curr = head;
+                head = tmp;
+            }
+            head = curr;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head = reverseKGroup(head, 2);
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+    }
+}
